9c5801e37590ee99ce17437dea521f1c48572b50
samc200577 <samuel.calderonc@uqvirtual.edu.co>
2025-11-21 07:13:30 -0500

    update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.parcial3_p2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.parcial3_p2 to javafx.fxml;
+    exports co.edu.uniquindio.poo.parcial3_p2;
+}
